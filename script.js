d1882eeb9ca1803affb85c31c211aa3410cf18ac
ShrastiJain <shrastijain1305@gmail.com>
2025-12-30 17:06:14 +0530

    Final Changes done

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-gsap.to(".section2 h2",{
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.matchMedia({
+
+  // ✅ Desktop (PIN works)
+  "(min-width: 769px)": function () {
+    gsap.to(".section2 h2",{
   x:-1500,
   scrollTrigger:{
     trigger:".section2",
@@ -9,3 +15,21 @@ gsap.to(".section2 h2",{
     pin:true
   }
 })
+  },
+
+  // ✅ Mobile (NO pin)
+  "(max-width: 768px)": function () {
+    
+    gsap.to(".section2 h2",{
+  x:-450,
+  scrollTrigger:{
+    trigger:".section2",
+    scroller:"body",
+    start:"top 0%",
+    end:"top -120%",
+    scrub:2,
+    pin:true
+  }
+})
+  }
+});
