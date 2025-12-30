9a7610838be745a778c6c746494e02167d25acb8
ShrastiJain <shrastijain1305@gmail.com>
2025-12-30 16:49:34 +0530

    Final Changes

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-gsap.registerPlugin(ScrollTrigger);
-
-ScrollTrigger.matchMedia({
-
-  // ✅ Desktop
-  "(min-width: 769px)": function () {
-    gsap.to(".section2 h2", {
-      xPercent: -70,
-      scrollTrigger: {
-        trigger: ".section2",
-        start: "top top",
-        end: "+=600",
-        scrub: 2,
-        pin: true
-      }
-    });
-  },
-
-  // ✅ Mobile (NO pin → no white space)
-  "(max-width: 768px)": function () {
-    gsap.to(".section2 h2", {
-      xPercent: -100,
-      scrollTrigger: {
-        trigger: ".section2",
-        start: "top top",
-        end: "+=300",
-        scrub: 1
-      }
-    });
+gsap.to(".section2 h2",{
+  x:-1500,
+  scrollTrigger:{
+    trigger:".section2",
+    scroller:"body",
+    start:"top 0%",
+    end:"top -150%",
+    scrub:2,
+    pin:true
   }
-});
-
-// final refresh
-ScrollTrigger.refresh();
+})
